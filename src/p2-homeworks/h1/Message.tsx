b934aea37cb886fa13cf0cd7ed4d0c88989b982f
ToshaBY <anton.rybakou@gmail.com>
2022-03-30 22:27:57 +0300

    feat: homework1 is done

---
+++ src/p2-homeworks/h1/Message.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import s from './Message.module.css'
+
+type MessageType = {
+    avatar: string;
+    name: string;
+    message: string;
+    time: string;
+}
+
+export const Message: React.FC<MessageType> = (props) => {
+    return (
+        <div className={s.main}>
+            <img src={`${props.avatar}`} className={s.avatar} alt='avatar'/>
+            <div className={s.text_area}>
+                <div className={s.name}>{props.name}</div>
+                <div className={s.message}>{props.message}</div>
+                <div className={s.time}>{props.time}</div>
+            </div>
+        </div>
+    )
+}
