7aef29a004a7f23b8658f38c44ed7584a75a2a3e
ToshaBY <anton.rybakou@gmail.com>
2022-05-30 17:07:41 +0300

    feat: fix Error404 page

--- src/p2-homeworks/h5-rrd-v6/Pages.tsx
+++ src/p2-homeworks/h5-rrd-v6/Pages.tsx
@@ -21,7 +21,7 @@ function Pages() {
                 <Route path={PATH.JUNIOR} element={<Junior/>}/>
                 <Route path={PATH.JUNIOR_PLUS} element={<JuniorPlus/>}/>
 
-                <Route path={'/*'} element={<Error404/>}/>
+                <Route path={'*'} element={<Error404/>}/>
             </Routes>
         </div>
     )
