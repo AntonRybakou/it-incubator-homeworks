36cec4f565d47cdede89efdb49bcbb28031551c2
ToshaBY <anton.rybakou@gmail.com>
2022-05-30 16:32:52 +0300

    feat: HW5

--- src/p2-homeworks/h5-rrd-v6/Header.tsx
+++ src/p2-homeworks/h5-rrd-v6/Header.tsx
@@ -1,10 +1,27 @@
 import React from 'react'
+import style from './Header.module.css'
+import {NavLink} from "react-router-dom";
+import {PATH} from "./Pages";
 
 function Header() {
     return (
-        <div>
-            // add NavLinks
-
+        <div className={style.menu}>
+            <div className={style.title}>Level</div>
+            <div className={style.item}>
+                <NavLink to={PATH.PRE_JUNIOR}>
+                    <span>PreJunior</span>
+                </NavLink>
+            </div>
+            <div className={style.item}>
+                <NavLink to={PATH.JUNIOR}>
+                    <span>Junior</span>
+                </NavLink>
+            </div>
+            <div className={style.item}>
+                <NavLink to={PATH.JUNIOR_PLUS}>
+                    <span>Junior+</span>
+                </NavLink>
+            </div>
         </div>
     )
 }
